6b8eee386f01dfec737be0b8608a1f0dadcba7e7
khoi1312010 <th.khoi2010@gmail.com>
2023-10-29 15:03:27 +0700

    SPCK

--- SPCK.js
+++ SPCK.js
@@ -1,26 +1,10 @@
-let products = [];
-const getProducts = async () => {
-  const response = await fetch(
-    "https://652177f2a4199548356d3fa6.mockapi.io/products"
-  );
-  products = await response.json();
-};
+let productList = [];
 
-const addProduct = async () => {
-  const newProductRequest = {
-    name: nameElm.value,
-    price: Number(priceElm.value),
-    quantity: Number(quantityElm.value),
-  };
-  const response = await fetch(
-    "https://652177f2a4199548356d3fa6.mockapi.io/products",
-    {
-      method: "POST",
-      headers: { "content-type": "application/json" },
-      body: JSON.stringify(newProductRequest),
-    }
-  );
-  const newProduct = await response.json();
-  products = [...products, newProduct];
-  displayProducts();
-};
+const getProductList = () => {
+  fetch("https://mockapi.io/projects/652177f2a4199548356d3fa7").then((res) => {
+    return res.json()
+  }).then((res) => {
+    productList = res;
+  })
+}
+
