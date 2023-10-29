fd8e57ec16a5d4639e15cf67d2de9bd2b4e6eeb2
khoi1312010 <th.khoi2010@gmail.com>
2023-10-29 17:12:50 +0700

    ok

--- SPCK.js
+++ SPCK.js
@@ -1,10 +1,21 @@
-let productList = [];
-
-const getProductList = () => {
-  fetch("https://mockapi.io/projects/652177f2a4199548356d3fa7").then((res) => {
-    return res.json()
-  }).then((res) => {
-    productList = res;
-  })
+const productList = document.getElementById("productList");
+let bookList = [];
+const bookTemplate = document.getElementById('book-item');
+const renderBookList = () => {
+  bookList.forEach(product => {
+    const productItem = bookTemplate.content.cloneNode(true);
+    productItem.getElementById('book-image').src = product.image;
+    productItem.getElementById('book-name').textContent = product.name;
+    productItem.getElementById('book-price').number = product.price;
+    productList.appendChild(productItem);
+  });
 }
-
+const getBooks = () => {
+  fetch('https://652177f2a4199548356d3fa6.mockapi.io/products')
+    .then(response => response.json())
+    .then(data => {
+      bookList = data;
+      renderBookList()
+    });
+}
+getBooks()
